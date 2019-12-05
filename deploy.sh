2fb9782969a3f1779396df91ab1c1a1961b83a60
Nicolás Gebauer <negebauerm@gmail.com>
2019-12-05 13:49:39 -0300

    chore: update deploy command to use scripts in server

--- deploy.sh
+++ deploy.sh
@@ -5,25 +5,16 @@ if [[ -z $HOST ]]; then
   exit 1
 fi
 
-folder=notify-watcher
-destination=$HOST:$folder
-
-echo "# Creating $folder folder if necessary"
-if ! ssh $HOST stat $folder \> /dev/null 2\>\&1; then
-  ssh $HOST "mkdir $folder"
-fi
-
 echo "# Copying files"
-scp .env docker-compose.yml docker-compose.prod.yml $destination
+scp .env $HOST:
+scp docker-compose.yml $HOST:docker-compose.notify-watcher.yml
+scp docker-compose.prod.yml $HOST:docker-compose.notify-watcher.prod.yml
 
-echo "# Running deploy"
 ssh $HOST ' \
-  cd notify-watcher \
-  && test -f docker-compose.other.yml || echo -e "version: \"3\"\n" > docker-compose.other.yml \
-  && echo "# Pulling images" \
-  && docker-compose -f docker-compose.yml -f docker-compose.prod.yml -f docker-compose.other.yml pull \
+  echo "# Pulling images" \
+  && ./docker-compose-pull.sh \
   && echo "# Mounting containers" \
-  && docker-compose -f docker-compose.yml -f docker-compose.prod.yml -f docker-compose.other.yml up -d --remove-orphans \
+  && ./docker-compose-up.sh \
   && echo "# Pruning system" \
   && echo y | docker system prune \
   '
